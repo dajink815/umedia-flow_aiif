fb28356c385c6d252568cc18898d5b1ba2245cfd
김다진 <dajink815@uangel.com>
2022-08-24 11:10:27 +0900

    1. RmqSttStartReq 개발중

--- src/main/java/com/uangel/aiif/rmq/handler/aiwf/incoming/RmqSttStartReq.java
+++ src/main/java/com/uangel/aiif/rmq/handler/aiwf/incoming/RmqSttStartReq.java
@@ -5,10 +5,15 @@ import com.uangel.aiif.rmq.handler.RmqMsgSender;
 import com.uangel.aiif.session.CallManager;
 import com.uangel.aiif.session.model.CallInfo;
 import com.uangel.protobuf.Header;
-import com.uangel.protobuf.MediaStartReq;
 import com.uangel.protobuf.Message;
+import com.uangel.protobuf.SttStartReq;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author dajin kim
@@ -16,6 +21,14 @@ import org.slf4j.LoggerFactory;
 public class RmqSttStartReq {
     static final Logger log = LoggerFactory.getLogger(RmqSttStartReq.class);
     private static final CallManager callManager = CallManager.getInstance();
+    private static final ScheduledExecutorService executors
+            = Executors.newScheduledThreadPool(1,
+            new BasicThreadFactory.Builder()
+                    .namingPattern("SttStartReq-%d")
+                    .daemon(true)
+                    // 우선 순위 어떻게?
+                    .priority(Thread.MAX_PRIORITY)
+                    .build());
 
     public RmqSttStartReq() {
         // nothing
@@ -24,7 +37,7 @@ public class RmqSttStartReq {
     public void handle(Message msg) {
 
         Header header = msg.getHeader();
-        MediaStartReq req = msg.getMediaStartReq();
+        SttStartReq req = msg.getSttStartReq();
         // req check isEmpty
 
         RmqMsgSender sender = RmqMsgSender.getInstance();
@@ -48,9 +61,28 @@ public class RmqSttStartReq {
 
         // todo STT Start
 
+        int sttDur = req.getDuration();
+
+        sttConverter.start();
+
+        // RTP 수신
+
+        // RTP 수신 결과에 따라 Response
+
+
 
         // Send Success Response
         sender.sendSttStartRes(header.getTId(), callInfo);
+
+        // Schedule
+        executors.schedule(() ->{
+
+            sttConverter.stop();
+
+            // STT 결과 처리
+
+            // STT_Result
+        }, sttDur, TimeUnit.MILLISECONDS);
 
     }
 }
